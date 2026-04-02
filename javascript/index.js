6f6c6f96c4d98cb5dbd8960f6a5ee73d51b30168
Abrar Butt <abrarbuttmi7@gmail.com>
2026-04-02 19:38:45 +0500

    html id s added

--- javascript/index.js
+++ javascript/index.js
@@ -1,26 +1,35 @@
-const userInput = document.getElementById("nameInput");
-const surnameDisplay = document.getElementById("surnameDisplay");
+// const userInput = document.getElementById("nameInput");
+// const surnameDisplay = document.getElementById("surnameDisplay");
 
-let user = {
-  name: "John",
-  surname: "Smith",
+// let user = {
+//   name: "John",
+//   surname: "Smith",
 
-  get fullName() {
-    return `${this.name} ${this.surname}`;
-  },
+//   get fullName() {
+//     return `${this.name} ${this.surname}`;
+//   },
 
-  set fullName(value) {
-    const parts = value.split(" ");
-    this.name = parts[0] || "";
-    this.surname = parts[1] || "";
-  }
-};
+//   set fullName(value) {
+//     const parts = value.split(" ");
+//     this.name = parts[0] || "";
+//     this.surname = parts[1] || "";
+//   }
+// };
 
-function displaySurname() {
-  surnameDisplay.textContent = user.fullName;
+// function displaySurname() {
+//   surnameDisplay.textContent = user.fullName;
+// }
+
+// userInput.addEventListener("input", (event) => {
+//   user.fullName = event.target.value;
+//   displaySurname();
+// });
+
+let count = 0;
+
+
+
+function increment() {
+  count++;
+  document.getElementById("counter").textContent = count;
 }
-
-userInput.addEventListener("input", (event) => {
-  user.fullName = event.target.value;
-  displaySurname();
-});
