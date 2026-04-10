ebfc38cd1c738270da15b8b2a14e2ebbaa95c89b
Abrar Butt <abrarbuttmi7@gmail.com>
2026-04-11 01:34:34 +0500

    feat: add webpack configuration for development environment with asset handling

---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  devServer: {
+    static: "./dist",
+    hot: true,
+    watchFiles: ["./src/template.html"],
+    port: 8080,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
